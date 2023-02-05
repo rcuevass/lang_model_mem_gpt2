3c598c371d154a9f4feff1f777fa7542821e1ff0
rcuevass <rogercuevas.ca@gmail.com>
2023-02-05 16:48:32 -0500

    refactoring parsing code

--- extraction.py
+++ extraction.py
@@ -7,7 +7,7 @@ import logging
 import time
 from utils.logging import get_log_object
 from utils.calculators import calculate_perplexity
-import argparse
+from utils.parsers import parse_arguments, parse_commoncrawl
 import numpy as np
 from pprint import pprint
 import sys
@@ -40,30 +40,6 @@ def print_best(metric, samples, name1, scores1, name2=None, scores2=None, n=10):
         pprint(samples[idx])
         print()
         print()
-
-
-def parse_commoncrawl(wet_file):
-    """
-    Quick and ugly parsing of a WET file.
-    Tested for the May 2021 crawl.
-    """
-    with open(wet_file) as f:
-        lines = f.readlines()
-
-    start_idxs = [i for i in range(len(lines)) if "WARC/1.0" in lines[i]]
-
-    all_eng = ""
-
-    count_eng = 0
-    for i in range(len(start_idxs) - 1):
-        start = start_idxs[i]
-        end = start_idxs[i + 1]
-        if "WARC-Identified-Content-Language: eng" in lines[start + 7]:
-            count_eng += 1
-            for j in range(start + 10, end):
-                all_eng += lines[j]
-
-    return all_eng
 
 
 def main(top_k_int: int = 40, seq_len_int: int = 256, gpt2_size_str: str = 'gpt2-medium'):
@@ -209,15 +185,6 @@ def main(top_k_int: int = 40, seq_len_int: int = 256, gpt2_size_str: str = 'gpt2
     metric = scores["zlib"] / np.log(scores["XL"])
     print(f"======== top sample by ratio of Zlib entropy and XL perplexity: ========")
     print_best(metric, samples, "PPL-XL", scores["XL"], "Zlib", scores["zlib"])
-
-
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--N', type=int, default=1000, help="Number of samples to generate")
-    parser.add_argument('--batch-size', type=int, default=10, help="Batch size for generation")
-    parser.add_argument('--internet-sampling', action='store_true', help="condition the generation using commoncrawl")
-    parser.add_argument('--wet-file', type=str, default=None, help="path to a commoncrawl WET file")
-    return parser.parse_args(argv)
 
 
 if __name__ == '__main__':
